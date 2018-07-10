0fb55706e9ad24964adc7f566df1eff10e1cb248
Greco Rubio <greco.rubio@token.io>
2018-07-10 16:43:51 -0500

    New button API

--- app/src/main/resources/script.js
+++ app/src/main/resources/script.js
@@ -1,16 +1,56 @@
 "use strict";
 
 var elementId = "tokenAccessBtn";
+var tokenController;
+var button;
 
 function createButton() {
-    // Create button
-    window.Token.styleButton({
-        id: elementId,
+    // create TokenPopupController to handle Popup messages
+    tokenController = window.Token.createPopupController();
+
+    // get button placeholder element
+    var element = document.getElementById(elementId);
+
+    // create the button
+    button = window.Token.createTokenButton(element, {
         label: "Token Access",
-    }, bindButton); // execute bindButton when styling button is finished
+    });
+
+    // setup onLoad callback
+    tokenController.onLoad(function(controller) {
+        // execute bindButton when controller is ready
+        bindButton();
+    });
+
+    // setup onSuccess callback
+    tokenController.onSuccess(function(data) { // Success Callback
+        // build success URL
+        var successURL = "/fetch-balances"
+           + "?tokenId=" + window.encodeURIComponent(data.tokenId);
+        // navigate to success URL
+        window.location.assign(successURL);
+    });
+
+    // setup onError callback
+    tokenController.onError(function(error) { // Failure Callback
+        throw error;
+    });
 }
 
-function bindButton(button) {
+function bindButton() {
+    // bind the Token Button to the Popup Controller
+    tokenController.bindButtonClick(button, function(action) {
+        // Each time the button is clicked, a new tokenRequestUrl is created
+        getTokenRequestUrl(function(tokenRequestUrl) {
+            // Initialize popup using the tokenRequestUrl
+            action(tokenRequestUrl);
+        });
+    });
+    // enable button after binding
+    button.enable();
+}
+
+function getTokenRequestUrl(done) {
     var XHR = new XMLHttpRequest();
 
     //set up the access request
@@ -27,19 +67,8 @@ function bindButton(button) {
 
      // Define what happens on successful data submission
      XHR.addEventListener("load", function(event) {
-         button.bindAccessButton(
-             event.target.responseURL, // request token URL
-             function(data) { // success callback
-                 // build success URL
-                 var successURL = "/fetch-balances"
-                    + "?tokenId=" + window.encodeURIComponent(data.tokenId);
-                 // navigate to success URL
-                 window.location.assign(successURL);
-             },
-             function(error) { // fail callback
-                 throw error;
-             }
-         );
+         // execute callback once response is received
+         done(event.target.responseURL);
      });
 
     // Send the data; HTTP headers are set automatically
