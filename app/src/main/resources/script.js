c5d0b5f7b0fe9562a55d4923098949c81a8715b9
Greco Rubio <greco.rubio@token.io>
2018-07-10 18:18:00 -0500

    New Button API and type selector

--- app/src/main/resources/script.js
+++ app/src/main/resources/script.js
@@ -4,7 +4,49 @@ var elementId = "tokenAccessBtn";
 var tokenController;
 var button;
 
-function createButton() {
+function clean() {
+    if (button) {
+        button.destroy();
+        button = null;
+    }
+
+    if (tokenController && tokenController.destroy) {
+        tokenController.destroy();
+        tokenController = null;
+    }
+}
+
+function createRedirectButton() {
+    // clean up instances
+    clean();
+
+    // create TokenPopupController to handle Popup messages
+    tokenController = window.Token.createRedirectController();
+
+    // get button placeholder element
+    var element = document.getElementById(elementId);
+
+    // create the button
+    button = window.Token.createTokenButton(element, {
+        label: "Redirect Token Access",
+    });
+
+    // bind the Token Button to the Redirect Controller when ready
+    tokenController.bindButtonClick(button, function(action) {
+    // Each time the button is clicked, a new tokenRequestUrl is created
+        getTokenRequestUrl(function(tokenRequestUrl) {
+            // Initialize popup using the tokenRequestUrl
+            action(tokenRequestUrl);
+        });
+    });
+    // enable button after binding
+    button.enable();
+}
+
+function createPopupButton() {
+    // clean up instances
+    clean();
+
     // create TokenPopupController to handle Popup messages
     tokenController = window.Token.createPopupController();
 
@@ -13,13 +55,21 @@ function createButton() {
 
     // create the button
     button = window.Token.createTokenButton(element, {
-        label: "Token Access",
+        label: "Popup Token Access",
     });
 
     // setup onLoad callback
     tokenController.onLoad(function(controller) {
-        // execute bindButton when controller is ready
-        bindButton();
+        // bind the Token Button to the Popup Controller when ready
+        tokenController.bindButtonClick(button, function(action) {
+            // Each time the button is clicked, a new tokenRequestUrl is created
+            getTokenRequestUrl(function(tokenRequestUrl) {
+                // Initialize popup using the tokenRequestUrl
+                action(tokenRequestUrl);
+            });
+        });
+        // enable button after binding
+        button.enable();
     });
 
     // setup onSuccess callback
@@ -35,19 +85,6 @@ function createButton() {
     tokenController.onError(function(error) { // Failure Callback
         throw error;
     });
-}
-
-function bindButton() {
-    // bind the Token Button to the Popup Controller
-    tokenController.bindButtonClick(button, function(action) {
-        // Each time the button is clicked, a new tokenRequestUrl is created
-        getTokenRequestUrl(function(tokenRequestUrl) {
-            // Initialize popup using the tokenRequestUrl
-            action(tokenRequestUrl);
-        });
-    });
-    // enable button after binding
-    button.enable();
 }
 
 function getTokenRequestUrl(done) {
@@ -68,11 +105,34 @@ function getTokenRequestUrl(done) {
      // Define what happens on successful data submission
      XHR.addEventListener("load", function(event) {
          // execute callback once response is received
-         done(event.target.responseURL);
+         if (event.target.status === 200) {
+             done(event.target.responseURL);
+         }
      });
 
     // Send the data; HTTP headers are set automatically
     XHR.send(data);
 }
 
-createButton();
+function setupButtonTypeSelector() {
+    var selector = document.getElementsByName('buttonTypeSelector');
+    var selected;
+    for (var i = 0; i < selector.length; i++) {
+        if (selector[i].checked) {
+            selected = selector[i].value;
+        }
+        selector[i].addEventListener('click', function(e) {
+            var value = e.target.value;
+            if (value === selected) return;
+            if (value === 'popup') {
+                createPopupButton();
+            } else if (value === 'redirect') {
+                createRedirectButton();
+            }
+            selected = value;
+        });
+    }
+    createPopupButton();
+}
+
+setupButtonTypeSelector();
